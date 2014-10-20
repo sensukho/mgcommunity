25b18c93d8468d4d76c0d1690f2c43ac9b32333f
JC Gil <sensukho@gmail.com>
2014-10-19 23:52:26 -0500

    Install Sonata Bundles

--- web/app_dev.php
+++ web/app_dev.php
@@ -1,11 +1,16 @@
 <?php
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Debug\Debug;
+/*
+ * This file is part of the Sonata package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
-// If you don't want to setup permissions the proper way, just uncomment the following PHP line
-// read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
-//umask(0000);
+require_once __DIR__ . '/../app/bootstrap.php.cache';
+require_once __DIR__ . '/../app/AppKernel.php';
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
@@ -17,14 +22,12 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
-Debug::enable();
 
-require_once __DIR__.'/../app/AppKernel.php';
+$request = Sonata\PageBundle\Request\RequestFactory::createFromGlobals('host_with_path');
 
 $kernel = new AppKernel('dev', true);
-$kernel->loadClassCache();
-$request = Request::createFromGlobals();
+
 $response = $kernel->handle($request);
 $response->send();
+
 $kernel->terminate($request, $response);
